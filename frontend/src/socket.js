3f3e6e30f210db126dcb65beabc199294b14331b
Akbar <n.akbarjon05@gmail.com>
2025-08-06 16:33:00 +0300

    front update

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 let socket;
 export const initSocket = () => {
   if (!socket) {
-    socket = io('http://localhost:5000'); 
+    socket = io('https://collaborativepresentation.onrender.com'); 
   }
   return socket;
 }; 
